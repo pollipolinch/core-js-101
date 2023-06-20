4f80d5c5586db3bca07f7993f32118617622cb9e
pollipolinch <pollifriend@mail.ru>
2023-06-21 00:57:12 +0400

    feat: add solutions 05-objects

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,16 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  // throw new Error('Not implemented');
+  const newObj = {
+    height,
+    width,
+    getArea() {
+      return this.height * this.width;
+    },
+  };
+  return newObj;
 }
 
 
@@ -35,8 +43,10 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  // throw new Error('Not implemented');
+  const jsonArr = JSON.stringify(obj);
+  return jsonArr;
 }
 
 
@@ -51,8 +61,11 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  // throw new Error('Not implemented');
+  const newOb = JSON.parse(json);
+  Object.setPrototypeOf(newOb, proto);
+  return newOb;
 }
 
 
